50646d44d11da88fd4f9de256c01075f80773b35
DettoAncheMilo <nico.mila91@gmail.com>
2026-09-02 22:31:44 +0200

    Add files via upload

--- iot-box/app.js
+++ iot-box/app.js
@@ -163,8 +163,12 @@ function resetDashboard() {
   selectedDriverId = null;
   localStorage.removeItem('pit_driver_id');
   
-  // Forza l'azzeramento totale della grafica (scrive P-, Gap 0, ecc.)
-  updateDashboard([]);
+  // Forza l'azzeramento totale e manuale dei box grafici
+  document.getElementById('pos').innerText = 'P-';
+  document.getElementById('driverAhead').innerHTML = '--';
+  document.getElementById('driverBehind').innerHTML = '--';
+  document.getElementById('gap').innerText = '+0.000';
+  document.getElementById('myDriverNum').innerText = '--';
   
   // Riporta il bottone giallo su LOAD e nasconde lo STOP
   setButtonState('default');
